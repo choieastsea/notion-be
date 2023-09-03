c67ccf86ec99bc2509b00e3af96404d8d7832d49
choieastsea <choieastsea@gmail.com>
2023-09-03 16:29:52 +0900

    [FEAT] Pages & Tree table
    - DB schema 정의 (closure pattern)
    - migration 수행

--- api/pages/models.py
+++ api/pages/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Page(models.Model):
+    page_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    class Meta:
+        db_table = 'pages'
+
+
+class PageTree(models.Model):
+    ancestor = models.ForeignKey(
+        Page, related_name='ancestor_set', on_delete=models.CASCADE)
+    descendant = models.ForeignKey(
+        Page, related_name='descendant_set', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'page_trees'
